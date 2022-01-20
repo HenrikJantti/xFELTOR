f478ca6fc43d0691d09a53f034cafb49bcdeebc0
Henrik Jantti <hja048@uit.no>
2022-01-20 09:47:14 +0100

    More options for loading different datasets

--- xfeltor/load.py
+++ xfeltor/load.py
@@ -9,6 +9,9 @@ def open_feltordataset(
     chunks: Union[int, dict] = None,
     restart_indices: bool = False,
     probes: bool = False,
+    probe_id: str = "Probes",
+    probex: str = "px",
+    probey: str = "py",
     **kwargs: dict,
 ) -> xr.Dataset:
     """Loads FELTOR output into one xarray Dataset. Can load either a single
@@ -26,6 +29,16 @@ def open_feltordataset(
         http://xarray.pydata.org/en/stable/user-guide/dask.html#chunking-and-performance
     restart_indices: bool, optional
         if True, duplicate time steps from restared runs are kept
+    probes: bool, optional
+        if True, indicates that the dataset contains probes and associates values of the
+        x and y possition for each probe with the coresponding probe_id.
+        Also changes the combine option to "by_coords".
+    probe_id: str, optional
+        The coordinate to associate the x and y positions of the probes with.
+    probex: str, optional
+        The name of the variable in the dataset where the x positon of probes are stored.
+    probey: str, optional
+        The name of the variable in the dataset where the y positon of probes are stored.
     kwargs : optional
         Keyword arguments are passed down to `xarray.open_mfdataset`, which in
         turn passes extra kwargs down to `xarray.open_dataset`.
@@ -60,6 +73,6 @@ def open_feltordataset(
         ds.attrs[i] = input_variables[i]
 
     if probes is True:
-        ds = ds.assign_coords(dict(probex=("Probes",ds.px.values),probey=("Probes",ds.py.values)))
+        ds = ds.assign_coords(dict(probex=(probe_id,ds[probex].values),probey=(probe_id,ds[probey].values)))
 
     return ds.isel(time=index)
